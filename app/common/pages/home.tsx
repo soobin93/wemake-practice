0323e425b6f14ab5eafc72af4ab2bb858185858f
Soobin Son <ssbin93@gmail.com>
2025-02-24 20:41:26 +1000

    downgrade to React 18

--- app/common/pages/home.tsx
+++ app/common/pages/home.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/common/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      Home <Button>Click me</Button>
+    <div className="flex justify-center items-center">
+      Home{" "}
+      <Button className="transition-colors bg-blue-500 hover:bg-blue-700 text-white">
+        Click me
+      </Button>
     </div>
   );
 }
